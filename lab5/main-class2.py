3b9800257c6c8a52ee284c19e1db9537e4fe9c2b
Rezwoan <frezwoan@gmail.com>
2026-03-08 12:00:21 +0600

    labTask1 snakeGame and Lab5

---
+++ lab5/main-class2.py
@@ -0,0 +1,69 @@
+# class BankAccount:
+#     def __init__(self, balance=0):
+#         self.__balance = balance
+    
+#     def deposit(self, amount):
+#         self.__balance += amount
+        
+#     def show_balance(self):
+#         print(f"Current balance: {self.__balance}")
+        
+# account = BankAccount()
+# account.deposit(100)
+# account.__balance = 200
+# account.show_balance()
+
+# class Animal:
+#     def eat(self):
+#         print("This Animal is eating")
+        
+# class Dog(Animal):
+#     def bark(self):
+#         print("The Dog is barking")
+
+# class Cat(Animal):
+#     def meow(self):
+#         print("The Cat is meowing")
+        
+# d1 = Dog()
+# d1.eat()
+# d1.bark()
+
+# c1 = Cat()
+# c1.eat()
+# c1.meow()
+
+# class A:
+#     def method_a(self):
+#         print("This is method A")
+    
+# class B(A):
+#     def method_b(self):
+#         print("This is method B")
+        
+# class C(B):
+#     def method_c(self):
+#         print("This is method C")
+
+# c1 = C()
+# c1.method_a()
+
+
+# class Father:
+#     def skills1(self):
+#         print("Programming")
+
+# class Mother:
+#     def skills2(self):
+#         print("Cooking")
+        
+        
+# class Child(Father, Mother):
+#     def skills3(self):
+#         print("Dancing")
+        
+# c1 = Child()
+# c1.skills1()
+# c1.skills2()
+# c1.skills3()
+
